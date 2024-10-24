b883ed06c0da073368618fa86d0aeafc7d2c3c6a
ttd3v <theo.dev.1234567890@gmail.com>
2024-10-24 11:31:08 -0300

    v0.0.3

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
-import secrets
-
+import smtplib
+import secrets,json
+from flask_cors import CORS
 app = Flask(__name__)
 app.secret_key = b'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+CORS(app, resources={r"/*": {"origins": "http://localhost:4200"}})
 db = SQLAlchemy(app)
+email_author = "theogamer560yt@gmail.com"
+email_password = "cted fics ohws fjxj"
 
 def getAuthKey():
     key = ''
@@ -31,17 +35,38 @@ def new_user():
         'ok':False,
         'auth_key':''
     }
-    data = request.data
+    data = json.loads(request.data)
     if data and 'username' in data and 'email' in data and 'password' in data:
+        TO = [data['email']]
+        subject = "Hello!"
+        text = 'Hi, welcome to VirtualWriter!'
+        message = """\
+        From: %s
+        To: %s
+        Subject: %s
+
+        %s
+        """ % (email_author, ", ".join(TO), subject, text)
         try:
-            USER = user(email=data['email'],username=data['username'],password=data['password'])
-            return_data['ok'] = True
-            return_data['auth_key'] = USER.AuthKey
-            db.session.add(USER)
-            print("A NEW USER HAS BEEN CREATED")
-        except Exception:
-            print(Exception)
-            db.session.rollback()
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(email_author,email_password)
+            server.sendmail(email_author,TO,message)
+            server.quit()
+            try:
+                USER = user(email=data['email'],username=data['username'],password=data['password'])
+                return_data['ok'] = True
+                return_data['auth_key'] = USER.AuthKey
+                return_data['username'] = USER.username
+                return_data['age'] = 0
+                db.session.add(USER)
+                print("A NEW USER HAS BEEN CREATED")
+            except Exception:
+                print(Exception)
+                db.session.rollback()
+        except Exception as err:
+            print(err)
+    
     return return_data
 
 if __name__ == "__main__":
--- virtual_writer/src/app/user/register/register.component.ts
+++ virtual_writer/src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { response } from 'express';
+import { error } from 'node:console';
+import { EventEmitter } from 'node:stream';
 
 @Component({
   selector: 'app-register',
@@ -8,5 +11,74 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  email = ''
+  username = ''
+  password = ''
+  isvalid_email(event : Event) : void{
+    if (event.target !== null){
+      let input : HTMLInputElement = (event.target as HTMLInputElement)
+      if (typeof input){
+        let value : string = String(input.value);
+        if (value.length > 0){
+          const regex = /^[^\s@]{5,}@[^\s@]+\.[^\s@]+$/;
+          const valid : boolean = regex.test(value);
+          input.className = valid? 'valid_input' : 'invalid_input';
+          this.email = input.value;
+        }else{
+          input.className = ""
+        }
+      }
+    }
+  }
+  isvalid_username(event : Event) : void{
+    if (event.target !== null){
+    let input : HTMLInputElement = (event.target as HTMLInputElement);
+    let content : string = String(input.value)
+    input.className = (content.length > 4 && content.length <= 20 && content.split(' ').length === 1)?'valid_input':'invalid_input';
+    this.username = input.value
+    if (content.length === 0){
+      input.className = ''
+    }
+  }
+  }
+  isvalid_password(event : Event) : void{
+    if (event.target !== null){
+      let input : HTMLInputElement = (event.target as HTMLInputElement);
+      this.password = input.value
+      input.className = String(input.value).length >= 8? 'valid_input' : 'invalid_input'
+    }
+  }
+  isvalid_confirmpassword(event : Event){
+    if (event.target !== null){
+      let input : HTMLInputElement = (event.target as HTMLInputElement);
+      input.className = (String(input.value).length >= 8 && input.value === this.password)? 'valid_input' : 'invalid_input'
+    }
+  }
+  submit(){
+    if(document.getElementsByClassName('valid_input').length === 4){
+      fetch("http://127.0.0.1:5000/newuser",{
+        method: "POST",
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({'email':this.email,'password':this.password,'username':this.username})
+      })
+      .then(response => {
+        if (!response.ok){
+          console.error(`Response wasn't ok | Status:${response.status} - StatusText:${response.statusText}`)
+        }
+        return response
+        
+      })
+      .then(response => response.json())
+      .then(data => {
+        if ('ok' in data && data['ok'] === true){
+          localStorage.setItem('auth_key',data['auth_key']);
+          localStorage.setItem('username',data['username']);
+          localStorage.setItem('age',data['age']);
+          window.location.replace('http://localhost:4200/dashboard')
+        }else if('ok' in data && data['ok'] === false){
+          alert("fail")
+        }
+      })
+    }
+  }
 }
